f0c121fc79c0df5b7af2da0374011215c8d95f08
xYFreezyYx <raimis.ozolins@gmail.com>
2022-11-10 19:07:23 +0200

    V2

--- Coms/main.js
+++ Coms/main.js
@@ -1,7 +1,14 @@
 var modal = document.getElementById('Login-form');
+var modal1 = document.querySelector("#Zoom-in")
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
   }
 }
