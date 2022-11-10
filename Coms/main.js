0c452cc496fb3ecd59694dc671d863331e5e00aa
xYFreezyYx <raimis.ozolins@gmail.com>
2022-11-10 22:03:28 +0200

    V3

--- Coms/main.js
+++ Coms/main.js
@@ -1,14 +1,31 @@
 var modal = document.getElementById('Login-form');
-var modal1 = document.querySelector("#Zoom-in")
+var modal1 = document.querySelector("#Zoom-in");
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
 
-window.onclick = function(event) {
   if (event.target == modal1) {
     modal1.style.display = "none";
+  }
+}
+
+let info = document.querySelector(".Info");
+let files = document.querySelector(".All-Files");
+let Flist = document.querySelector(".F-List");
+
+function Prgoramming(){
+  info.style.display = "none";
+  files.style.display = "flex";
+}
+
+function FriendsList(){
+  if(Flist.id == "Closed"){
+    Flist.id = "Open";
+    Flist.style.marginRight = "0px";
+  }else if(Flist.id == "Open"){
+    Flist.id = "Closed";
+    Flist.style.marginRight = "-250px";
   }
 }
